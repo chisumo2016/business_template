6546eaef603136f3293f2a1b596c89d002c0d997
Bernard <bchisumo74@gmail.com>
2019-04-29 16:40:10 +0100

    Implemented the Animated text with CSS

--- js/style_js.js
+++ js/style_js.js
@@ -1,10 +1,21 @@
 
-$( document ).ready(function() {
+jQuery().ready(function () {
     "use strict"
-
     $('.slider').ripples({
         dropRadius: 13,
         perturbance: 0.01
 
     });
+
+
+    $(".text").typed({
+        strings: [" <strong> i love </strong> <strong class='primary'>codings .</strong>", "<strong>and to </strong>  <strong class='primary'>share !!</strong>"],
+        typeSpeed: 0,
+        loop:true
+
+    });
+
 });
+
+
+
