630d1cf3ba18d7fac0d55457df88cf259263dd8a
Bernard <bchisumo74@gmail.com>
2019-04-30 11:07:48 +0100

    Implemented the Team Members Successfully

--- js/style_js.js
+++ js/style_js.js
@@ -36,6 +36,28 @@ jQuery().ready(function () {
         }
     });
 
+    $("#team-members").owlCarousel({
+        items:3,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoplayHoverPause:true,
+        responsive:{
+
+            0:{
+                items:1
+            },
+
+            480:{
+                items:1
+            },
+
+            768:{
+                items:3
+            }
+        }
+    });
+
 });
 
 
