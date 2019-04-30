ffc7151b8404601abfc2f31577f3809407b81782
Bernard <bchisumo74@gmail.com>
2019-04-30 07:14:45 +0100

    Service Section Completed Successfully

--- js/style_js.js
+++ js/style_js.js
@@ -1,22 +1,35 @@
 
-$( document ).ready(function() {
+jQuery().ready(function () {
     "use strict"
-
     $('.slider').ripples({
         dropRadius: 13,
         perturbance: 0.01
 
     });
 
-    $(window).scroll(function () {
+
+    $(".text").typed({
+        strings: [" <strong> i love </strong> <strong class='primary'>codings .</strong>", "<strong>and to </strong>  <strong class='primary'>share !!</strong>"],
+        typeSpeed: 0,
+        loop:true
+
+    });
+
+    $(window).scroll(function() {
+
         var top = $(window).scrollTop();
         if(top >=60){
+
             $("nav").addClass('secondary')
         }else
-            if($("nav").hasClass('secondary')){
-                $("nav").removeClass('secondary')
-            }
+        if($("nav").hasClass('secondary')){
+            $("nav").removeClass('secondary');
+        }
 
 
     });
+
 });
+
+
+
